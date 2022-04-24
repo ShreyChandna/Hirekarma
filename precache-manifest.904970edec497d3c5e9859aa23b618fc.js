0e454f95639c30297a6dfce5241f85695304216a
Shrey Chandna <84912943+ShreyChandna@users.noreply.github.com>
2022-04-24 12:16:34 +0530

    Updates

---
+++ precache-manifest.904970edec497d3c5e9859aa23b618fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f98311a5b9b17fa6404b1a3f2f58a86b",
+    "url": "/Hirekarma/index.html"
+  },
+  {
+    "revision": "aefde5db264a398924f0",
+    "url": "/Hirekarma/static/css/main.304e716e.chunk.css"
+  },
+  {
+    "revision": "a742755305512407baff",
+    "url": "/Hirekarma/static/js/2.78b4ec1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Hirekarma/static/js/2.78b4ec1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aefde5db264a398924f0",
+    "url": "/Hirekarma/static/js/main.46f48907.chunk.js"
+  },
+  {
+    "revision": "2d0c23a5e3bc25247169",
+    "url": "/Hirekarma/static/js/runtime-main.35a9a5c6.js"
+  }
+]);
